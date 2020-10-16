7486681be246f58ff74f66483f18749e9dff9638
Sergey Dolgikh <ageres7@gmail.com>
2020-10-16 10:36:48 +0500

    Initial Commit

---
+++ HM.2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM.2.2
+//
+//  Created by Сергей Долгих on 16.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
