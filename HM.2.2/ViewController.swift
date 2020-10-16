fd3b6b3f6b3067ca0c4a6c7e8821c8a28f0b1526
Sergey Dolgikh <ageres7@gmail.com>
2020-10-16 15:34:32 +0500

    Working!

--- HM.2.2/ViewController.swift
+++ HM.2.2/ViewController.swift
@@ -19,22 +19,45 @@ class ViewController: UIViewController {
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
     
-    private let currentColor = UIColor(displayP3Red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+    
+    private var currentRedValue: CGFloat = 0.50
+    private var currentGreenValue: CGFloat = 0.50
+    private var currentBlueValue: CGFloat = 0.50
+    
+    private var currentColor = UIColor()
+    
+//    private let currentColor = UIColor(displayP3Red: 50, green: 0.50, blue: 0.50, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        currentColor = UIColor(displayP3Red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+        
         colorView.layer.cornerRadius = 15
         colorView.backgroundColor = currentColor
-        
+    
     }
 
     @IBAction func redSliderAction() {
+        currentRedValue = CGFloat(redSlider.value)
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        
+        setColor()
     }
     @IBAction func greenSliderAction() {
+        currentGreenValue = CGFloat(greenSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        setColor()
     }
     @IBAction func blueSliderAction() {
+        currentBlueValue = CGFloat(blueSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        setColor()
     }
     
+    func setColor() {
+        currentColor = UIColor(displayP3Red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+        colorView.backgroundColor = currentColor
+    }
 }
 
